6af2b800aafa7b6781843a65fd8647e2ecd23dbb
Vladislav Krasheninnikov <139855754+Puncherschance@users.noreply.github.com>
2023-09-02 10:47:43 +0400

    Добавлены Login Page методы

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 
@@ -6,5 +7,9 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     page.should_be_login_link()
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page = LoginPage(browser, link)
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
